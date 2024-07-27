16c8c12f27602a9d9ceead74eb690de31a1751ad
Aditi <92170627+Aditi020@users.noreply.github.com>
2024-07-27 11:17:29 +0530

    Create Palindrome.cpp

---
+++ Palindrome.cpp
@@ -0,0 +1,43 @@
+// Given a string, find the minimum number of characters to be inserted to convert it to a palindrome.
+
+// Examples :
+
+// Input: str = "abcd"
+// Output: 3
+// Explanation: Inserted character marked with bold characters in dcbabcd, here we need minimum three characters to make it palindrome.
+// Input: str = "aa"
+// Output: 0
+// Explanation: "aa" is already a palindrome.
+
+class Solution{
+  public:
+
+    int lps(string s1 , string s2 , int i , int j , vector<vector<int>> &dp){
+
+        if(i >= s1.length() || j >= s2.length()){
+            return 0;
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+
+        if(s1[i] == s2[j]){
+            return dp[i][j] = 1 + lps(s1,s2,i+1,j+1,dp);
+        }
+
+        int a1 = lps(s1,s2,i+1,j,dp);
+        int a2 = lps(s1,s2,i,j+1,dp);
+        return dp[i][j] = max(a1,a2);
+
+    }
+
+    int countMin(string str){
+
+        int n = str.length();
+        string s1 = str;
+        reverse(str.begin(),str.end());
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        return n - lps(s1,str,0,0,dp);
+    }
+};
