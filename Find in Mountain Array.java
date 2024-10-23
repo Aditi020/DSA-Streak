a037a8ce47b6491974e49280bfb9f7049def9b0a
Aditi <92170627+Aditi020@users.noreply.github.com>
2024-10-24 02:53:04 +0530

    Update Find in Mountain Array.java

--- Find in Mountain Array.java
+++ Find in Mountain Array.java
@@ -10,22 +10,64 @@
 
 
  
-class Solution {
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+   class Solution {
     public int findInMountainArray(int target, MountainArray mountainArr) {
-       int start=0,end= mountainArr.length()-1; int result=-1;
+        int peakIndex = findPeakElement(mountainArr);
+        int res = BS(mountainArr, target, 0, peakIndex);
+        if (res != -1) {
+            return res;
+        }
+        return BS2(mountainArr, target, peakIndex + 1, mountainArr.length() - 1);
+    }
+
+    public static int findPeakElement(MountainArray mountainArr) {
+        int start = 0, end = mountainArr.length() - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (mountainArr.get(mid) > mountainArr.get(mid + 1)) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+
+    public static int BS(MountainArray mountainArr, int target, int start, int end) {
         while (start <= end) {
             int mid = start + (end - start) / 2;
-
             if (mountainArr.get(mid) == target) {
-                result = mid;  // Store the index
-                end = mid - 1;    // Keep searching the left half
+                return mid;
             } else if (mountainArr.get(mid) < target) {
                 start = mid + 1;
             } else {
-                end = mid-1;
+                end = mid - 1;
             }
         }
-        return result;   
+        return -1;
+    }
+
+    public static int BS2(MountainArray mountainArr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mountainArr.get(mid) == target) {
+                return mid;
+            } else if (mountainArr.get(mid) > target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
     }
 }
 
@@ -33,34 +75,76 @@ class Solution {
 // or
 
 
-// complete solution below 
-// import java.util.Arrays;
-// import java.math.*;
-
-// class Solution {
-//     public static void main(String[] args) {
-//         int[] arr = {1,2,3,4,5,3,1};
-//         System.out.println(BS(arr, 3));  // Should print the index of 79
-//     }
-
-//     // Binary Search function within a specific range
-//     public static int BS(int[] nums, int target ) {
-//         int start=0,end= nums.length-1; int result=-1;
-//         while (start <= end) {
-//             int mid = start + (end - start) / 2;
-
-//             if (nums[mid] == target) {
-//                 result = mid;  // Store the index
-//                 end = mid - 1;    // Keep searching the left half
-//             } else if (nums[mid] < target) {
-//                 start = mid + 1;
-//             } else {
-//                 end = mid-1;
-//             }
-//         }
-//         return result;
-//      }
-//     }
+//COMPLETE NORMAL SOLUTION 
 
 
 
+import java.util.Arrays;
+import java.math.*;
+
+class Solution {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 2};
+        System.out.println(search(arr, 2));  // Should print the index of 79
+    }
+
+    public static int findPeakElement(int[] nums) {
+        int start = 0, end = nums.length - 1;
+        while (start < end) {
+
+            int mid = start + (end - start) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+
+    public static int search(int[] arr, int target) {
+        int a = findPeakElement(arr);
+        int res = 0;
+            res = BS(arr, 2, a);
+
+         if (res != -1) {
+            return res; // Found in the left side
+        }
+        return BS2(arr, 2, a+1);
+
+    }
+
+    // Binary Search function within a specific range
+    public static int BS(int[] nums, int target, int end) {
+        int start=0;
+        while (start <= end) {
+            int mid = start + (end - start) / 2; // Calculate mid index
+
+            if (nums[mid] == target) {
+                return mid; // Target found
+            } else if (nums[mid] < target) {
+                start = mid + 1; // Move to the right half
+            } else {
+                end = mid - 1; // Move to the left half
+            }
+        }
+        return -1; // Target not found
+    }
+
+    // Binary Search function for the right side (descending part)
+    public static int BS2(int[] nums, int target, int start) {
+        int end= nums.length-1;;
+        while (start <= end) {
+            int mid = start + (end - start) / 2; // Calculate mid index
+
+            if (nums[mid] == target) {
+                return mid; // Target found
+            } else if (nums[mid] < target) {
+                end = mid - 1; // Move to the left half (decrease end)
+            } else {
+                start = mid + 1; // Move to the right half (increase start)
+            }
+        }
+        return -1;
+    }
+}
