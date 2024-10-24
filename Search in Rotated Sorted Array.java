72577d61c2740dde0d2bc121955bd7f99f10c5f0
Aditi <92170627+Aditi020@users.noreply.github.com>
2024-10-24 23:36:15 +0530

    Create Search in Rotated Sorted Array.java

---
+++ Search in Rotated Sorted Array.java
@@ -0,0 +1,11 @@
+// Given a **rotated sorted array** in ascending order with distinct values, write an algorithm that returns the **index of a target element** if it exists, or **-1 if it does not**. The rotation occurs at an unknown pivot, dividing the array into two sorted parts. Ensure the solution runs in **O(log n)** time.
+//   Example 1:
+// Input: nums = [4,5,6,7,0,1,2], target = 0
+// Output: 4
+// Example 2:
+// Input: nums = [4,5,6,7,0,1,2], target = 3
+// Output: -1
+// Example 3:
+// Input: nums = [1], target = 0
+// Output: -1
+
