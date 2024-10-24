849aeb503c68412fd5ff80befb7f3e4c4249c39a
Aditi <92170627+Aditi020@users.noreply.github.com>
2024-10-25 01:05:39 +0530

    Update Search in Rotated Sorted Array.java

--- Search in Rotated Sorted Array.java
+++ Search in Rotated Sorted Array.java
@@ -9,3 +9,46 @@
 // Input: nums = [1], target = 0
 // Output: -1
 
+class Solution {
+    public int search(int[] nums, int target) {
+        int a = Pivot(nums);
+        int res = 0;
+        res = BS(nums, target, 0, a-1);
+
+        if (res != -1) {
+            return res; // Found in the left side
+        }
+        return BS(nums, target, a, nums.length - 1);
+
+    }
+        public static int Pivot(int[] nums) {
+        int start = 0, end = nums.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] > nums[end]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+
+    // Binary Search function within a specific range in ascending order
+    public static int BS(int[] nums, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2; // Calculate mid index
+
+            if (nums[mid] == target) {
+                return mid; // Target found
+            } else if (nums[mid] < target) {
+                start = mid + 1; // Move to the right half
+            } else {
+                end = mid - 1; // Move to the left half
+            }
+        }
+        return -1;
+    }
+}
