9bbedeeafb3c3c6ff776838e03a849fc9f292c56
Aditi <92170627+Aditi020@users.noreply.github.com>
2024-10-25 03:29:50 +0530

    Update Search in Rotated Sorted Array(Duplicate values) II.java

--- Search in Rotated Sorted Array(Duplicate values) II.java
+++ Search in Rotated Sorted Array(Duplicate values) II.java
@@ -6,3 +6,71 @@
 // Input: nums = [2,5,6,0,0,1,2], target = 3
 // Output: false
 
+class Solution {
+    public boolean search(int[] nums, int target) {
+       int a = PivotforDuplicates(nums);
+//     System.out.println(a);
+
+        if (BS(nums, target, 0, a ) != -1) {
+            return true; // Found in the left side
+        }
+        // Search in the right side of the pivot
+        return BS(nums, target, a+1, nums.length - 1) != -1;
+    }
+
+      public static int PivotforDuplicates(int[] arr) {
+ int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            // 4 cases over here
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+
+            // if elements at middle, start, end are equal then just skip the duplicates
+            if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
+                // skip the duplicates
+                // NOTE: what if these elements at start and end were the pivot??
+                // check if start is pivot
+                if (start < end && arr[start] > arr[start + 1]) {
+                    return start;
+                }
+                start++;
+
+                // check whether end is pivot
+                if (end > start && arr[end] < arr[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+            }
+            // left side is sorted, so pivot should be in right
+            else if (arr[start] < arr[mid] || (arr[start] == arr[mid] && arr[mid] > arr[end])) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+      }
+
+    // Binary Search function within a specific range in ascending order
+    public static int BS(int[] nums, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2; // Calculate mid index
+
+            if (nums[mid] == target) {
+                return mid; // Target found
+            } else if (nums[mid] < target) {
+                start = mid + 1; // Move to the right half
+            } else {
+                end = mid - 1; // Move to the left half
+            }
+        }
+        return -1;
+    }
+}
+
