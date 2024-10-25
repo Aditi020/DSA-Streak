d69fca3d18a22caf4202042f4764b7388b44d0de
Aditi <92170627+Aditi020@users.noreply.github.com>
2024-10-25 23:48:50 +0530

    Update Search a 2D Matrix.java

--- Search a 2D Matrix.java
+++ Search a 2D Matrix.java
@@ -9,3 +9,29 @@
 // Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
 // Output: false
 
+import java.util.Arrays;
+class Solution {
+    public static void main(String[] args) {
+        int[][] arr =
+        {{1 ,3 ,5 ,7},
+        {10,11,16,20},
+        {23,30,34,60}};
+        System.out.println(Arrays.toString(search(arr, 60)));  // Should print the index of 79
+    }
+
+    static int[] search(int[][] matrix, int target) {
+        int r = 0;
+        int c = matrix[r].length-1;
+        while (r < matrix.length && c >= 0) {
+            if (matrix[r][c] == target) {
+                return new int[]{r, c};
+            }
+            if (matrix[r][c] < target) {
+                r++;
+            } else {
+                c--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
