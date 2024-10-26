81897a6e51c16348acc88516b71185eba36388b7
Aditi <92170627+Aditi020@users.noreply.github.com>
2024-10-27 03:07:18 +0530

    Update Set Mismatch.java

--- Set Mismatch.java
+++ Set Mismatch.java
@@ -6,3 +6,40 @@
 // Input: nums = [1,1]
 // Output: [1,2]
 
+class Solution {
+    public int[] findErrorNums(int[] arr) {
+    int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;  // Expected position for current element
+
+            // Only swap if the current element is not at the correct position,
+            // and the target position doesn't already have the correct value
+            if (arr[i] != arr[correct] && arr[i] <= arr.length) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        int duplicate = -1;
+        int missing = -1;
+
+        // Collect the duplicate and missing numbers
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] != j + 1) {
+                duplicate = arr[j]; // The number which is out of place is the duplicate
+                missing = j + 1;    // The expected number at this index
+                break; // We can break since we only need one duplicate
+            }
+        }
+
+        // Return the result as an array
+        return new int[]{duplicate, missing};
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
