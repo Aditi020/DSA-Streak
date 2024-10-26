3fd60f50f7c880f1dbdf1287ff00ac699c6abb32
Aditi <92170627+Aditi020@users.noreply.github.com>
2024-10-27 02:20:07 +0530

    Update Find the Duplicate Number.java

--- Find the Duplicate Number.java
+++ Find the Duplicate Number.java
@@ -9,4 +9,26 @@
 // Input: nums = [3,3,3,3,3]
 // Output: 3
 
-
+class Solution {
+    public int findDuplicate(int[] arr) {
+            int i = 0 ;
+            while (i < arr.length) {
+                int correct = arr[i]-1 ;
+                if (arr[i]< arr.length && arr[i] != arr[correct])
+                    swap(arr, i, correct);
+                 else
+                    i++;
+            }
+            for (int j = 0; j < arr.length ; j++) {
+                if(arr[j]!=j+1)
+                    return arr[j];
+            }
+            return arr[arr.length]+1;
+    }
+    
+        static void swap(int[] arr, int first, int second) {
+            int temp = arr[first];
+            arr[first] = arr[second];
+            arr[second] = temp;
+        }
+}
