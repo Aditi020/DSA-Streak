332975912d8cb3d2183a23e9a8d9e3bcac8fb65a
Aditi <92170627+Aditi020@users.noreply.github.com>
2025-05-22 01:28:20 +0530

    Create Kth Smallest Number in Multiplication Table.java

---
+++ Kth Smallest Number in Multiplication Table.java
@@ -0,0 +1,23 @@
+
+// Given three integers m, n, and k, representing an m × n multiplication table (1-indexed), return the kth smallest element from it. Each cell of the matrix is defined as:mat[i][j] = i × j
+// The multiplication table is filled in row-wise sorted and column-wise sorted order.
+// 📘 Examples
+// Example 1:
+// Input: m = 3, n = 3, k = 5
+// Output: 3
+// Example 2:
+// Input: m = 2, n = 3, k = 6
+// Output: 6
+
+class Solution {
+    public int kthSmallest(int m, int n, int k) {
+        int l = 1, r = m * n;
+        while (l < r) {
+            int mid = (l + r) / 2, cnt = 0;
+            for (int i = 1; i <= m; i++) cnt += Math.min(n, mid / i);
+            if (cnt < k) l = mid + 1;
+            else r = mid;
+        }
+        return l;
+    }
+}
